fb0baae0d0090bf1e7ef16298a2c38eb1fa7be0b
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2022-07-18 07:26:53 +0200

    add navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import blackLogo from "../public/assets/images/logo-adriane-m-fleuriste-paris-black.svg"
-import whiteLogo from "../public/assets/images/logo-adriane-m-fleuriste-paris-white.svg"
-import Hamburger from 'hamburger-react'
+import blackLogo from "../public/assets/images/logo-adriane-m-fleuriste-paris-black.svg";
+import whiteLogo from "../public/assets/images/logo-adriane-m-fleuriste-paris-white.svg";
+import Hamburger from "hamburger-react";
+import { GrMap } from "react-icons/gr";
 
 const Navbar = () => {
-
+  const [nav, setNav] = useState(false);
   const [isOpen, setOpen] = useState(false)
 
+
+  const handleNav = () => { 
+    setNav(!nav);
+  };
+
   return (
-    <div>
-      <div>
+    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+      <div className="max-w-[1600px] m-auto flex justify-between items-center p-4">
         <Link href="/">
           <Image
             src={blackLogo}
@@ -20,10 +26,48 @@ const Navbar = () => {
             width="320"
           />
         </Link>
-        <Hamburger toggled={isOpen} toggle={setOpen} />
-
+        <div className="flex justify-between items-center gap-6">
+          <div>
+            <GrMap size={35} />
+          </div>
+          <div onClick={handleNav} className="block z-10">
+          {nav ? (
+              <Hamburger toggled={isOpen} toggle={setOpen} size={30} color="#fff" rounded/>
+            ) : (
+              <Hamburger toggled={isOpen} toggle={setOpen} size={30} rounded/>
+            )}
+          </div>
+          <div
+            className={
+              nav
+                ? "absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center text-white transition-transform"
+                : "absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center text-white transition-transform"
+            }
+          >
+            <ul className="flex flex-col gap-20 text-4xl">
+              <li>
+                <Link href="/">Livraisons de fleurs</Link>
+              </li>
+              <li>
+                <Link href="/">Bouquet du mois</Link>
+              </li>
+              <li>
+                <Link href="/">Mariage</Link>
+              </li>
+              <li>
+                <Link href="/">Evènements</Link>
+              </li>
+              <li>
+                <Link href="/">Abonnements</Link>
+              </li> 
+              <li>
+                <Link href="/">Terrasses & Balcons</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div> 
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        "in-expo": "transform 1s cubic-bezier(1, 0, 0, 1)",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
+
